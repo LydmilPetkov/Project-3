6b947b0deb4f39c618b264a60ea4fca4eefb889b
Lyudmil <lyudmilpetkov92@gmail.com>
2025-06-20 12:19:00 +0200

    created all the components, added navbar, added footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Main } from './main/main';
+import { About } from './about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Main,
+  },
+  {
+    path: 'about',
+    component: About,
+  },
+];
