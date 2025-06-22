57fee8b09af409487474aea544cb1c707a0ae7e6
Lyudmil <lyudmilpetkov92@gmail.com>
2025-06-22 12:22:19 +0200

    added 10% service tax and a 15% discount above 10000 euros in the cart calculation, added additional designs to the website

--- src/app/cart/cart.ts
+++ src/app/cart/cart.ts
@@ -4,16 +4,65 @@ import { CartService } from '../cart-service';
 
 @Component({
   selector: 'app-cart',
-  imports: [],
   templateUrl: './cart.html',
   styleUrl: './cart.css',
 })
 export class Cart {
   cart: productInterface[] = [];
   total: number = 0;
-  item: any;
+  taxRate: number = 0.1;
+  taxAmount: number = 0;
+  totalWithTax: number = 0;
+  discountRate: number = 0.15;
+  discount: number = 0;
+  finalTotal: number = 0;
+  totalStr: string = '';
+  taxAmountStr: string = '';
+  totalWithTaxStr: string = '';
+  discountStr: string = '';
+  finalTotalStr: string = '';
+  qualifiesForDiscount: boolean = false;
+
   constructor(private cartService: CartService) {
     this.cart = this.cartService.getCart();
+    this.recalculateTotals();
+  }
+
+  recalculateTotals() {
     this.total = this.cartService.calcTotal();
+    this.taxAmount = this.total * this.taxRate;
+    this.totalWithTax = this.total + this.taxAmount;
+    this.qualifiesForDiscount = this.totalWithTax > 10000;
+    this.discount = this.qualifiesForDiscount
+      ? this.totalWithTax * this.discountRate
+      : 0;
+    this.finalTotal = this.totalWithTax - this.discount;
+    this.totalStr = this.total.toFixed(2);
+    this.taxAmountStr = this.taxAmount.toFixed(2);
+    this.totalWithTaxStr = this.totalWithTax.toFixed(2);
+    this.discountStr = this.discount.toFixed(2);
+    this.finalTotalStr = this.finalTotal.toFixed(2);
+  }
+
+  increaseQtty(item: productInterface) {
+    item.qtty++;
+    this.recalculateTotals();
+  }
+
+  decreaseQtty(item: productInterface) {
+    item.qtty--;
+    if (item.qtty < 1) {
+      this.removeItem(item);
+    }
+    this.recalculateTotals();
+  }
+
+  removeItem(item: productInterface) {
+    this.cart = this.cart.filter((i) => i.id !== item.id);
+    // Also update the cart in the service if needed
+    const serviceCart = this.cartService.getCart();
+    const idx = serviceCart.findIndex((i) => i.id === item.id);
+    if (idx > -1) serviceCart.splice(idx, 1);
+    this.recalculateTotals();
   }
 }
